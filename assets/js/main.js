feb23c226ec0b712360b7f5e33f71751afd5c507
ecoan <ecoandrea@gmail.com>
2026-05-20 21:10:25 -0400

    feat: primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const likeButtons = document.querySelectorAll(".btn-like");
+
+likeButtons.forEach((button) => { //recorre cada botón de like,  
+  button.addEventListener("click", () => {
+    const post = button.closest(".post"); //buscar el post más cercano al botón que se ha hecho clic
+    const likeCount = post.querySelector(".likes-count"); //busca el elemento que muestra el número de likes dentro del post
+
+    let count = parseInt(likeCount.textContent); //obtenemos el numero actual de likes y con parseInt se asegura que sea entero
+    count++;
+    likeCount.textContent = `${count} like(s)`; //actualizo texto
+  });
+});
